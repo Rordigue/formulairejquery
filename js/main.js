65ce500631d47559afcd85a0cec86d2887b8bf41
formateur <formateur@wf3.com>
2020-10-27 10:37:32 +0100

    add jquery version natif to jquery

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 // window.onload = function (){
 
 //     const form = document.querySelector('form');
-    const inputs = document.querySelectorAll('input');
-    const container = document.querySelector('#container')
 //     const body = document.querySelector('body');
 
 //     form.addEventListener('submit', (e) => {
@@ -38,41 +36,49 @@
 //         HTMLelementAppendTo.appendChild(pError);
 //     }
 // }
-
 $(document).ready(function(){
+    const inputs = $('input');
+    const container = $('#container')
+    console.log(inputs)
+    console.log(container)
     $(":submit").click(function(e){
         console.log('le bouton marche')
-        inputs.forEach((input,index) => {
-            if(input.name !== "valider"){
+        Object.keys(inputs)
+            .filter(key => Number.isInteger(parseInt(key,10)))
+            .forEach(key => {
+                    console.log(key)
+            console.log($(inputs[key]))
+            if($(inputs[key]).attr('name') !== "valider"){
                 console.log('salut ça va')
-                if(input.value.trim() === ""){
-                    console.log('oui et toi ?')
+                console.log('value : ', $(inputs[key]).eq(0).val())
+                if($(inputs[key]).val().trim() === ""){
                     const divError = createDivError(container)
-                    createMessageError(divError, `Le champ ${input.name} n'a pas été remplis`)
+                    createMessageError(divError, `Le champ ${$(inputs[key]).attr('name')} n'a pas été remplis`)
                 }else{
 
                 }
+                    
             }
         })
         e.preventDefault();
     });
 
     const createDivError = function(HTMLelementAppendTo){
-        const divError = document.createElement('div')
-        $(".divError").attr("class", "error")
+        const divError = $('<div></div>')
+        divError.addClass("error")
 
-        HTMLelementAppendTo.appendChild(divError)
-        HTMLelementAppendTo.classList.add('column-reverse')
-        const divErrorDom = document.querySelector('.error')
+        HTMLelementAppendTo.append(divError)
+        HTMLelementAppendTo.addClass('column-reverse')
+        const divErrorDom = $('.error')
         return divErrorDom
     }
 
     const createMessageError = function(HTMLelementAppendTo, messageError){
-        const pError = document.createElement('p');
-        const textError = document.createTextNode(messageError);
+        const pError = $('<p></p>');
+        const textError = pError.text(messageError)
 
-        pError.appendChild(textError);
+        pError.append(textError);
 
-        HTMLelementAppendTo.appendChild(pError);
+        HTMLelementAppendTo.append(pError);
     }
 });
