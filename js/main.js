c1e7e8f9a12285b561c97ec932cf88da369b2d6d
Rodrigue Queraud <queraudrodrigue@outlook.fr>
2020-10-27 10:15:12 +0100

    form refait a ma maniere

--- js/main.js
+++ js/main.js
@@ -1,78 +1,29 @@
-// window.onload = function (){
-
-//     const form = document.querySelector('form');
-    const inputs = document.querySelectorAll('input');
-    const container = document.querySelector('#container')
-//     const body = document.querySelector('body');
-
-//     form.addEventListener('submit', (e) => {
-//         inputs.forEach((input,index) => {
-//             if(input.name !== "valider"){
-//                 if(input.value.trim() === ""){
-//                     const divError = createDivError(container)
-//                     createMessageError(divError, `Le champ ${input.name} n'a pas été remplis`)
-//                 }else{
-
-//                 }
-//             }
-//         })
-//         e.preventDefault();
-//     }, false);
-
-//     const createDivError = function(HTMLelementAppendTo){
-//         const divError = document.createElement('div')
-//         divError.setAttribute('class', 'error')
-
-//         HTMLelementAppendTo.appendChild(divError)
-//         HTMLelementAppendTo.classList.add('column-reverse')
-//         const divErrorDom = document.querySelector('.error')
-//         return divErrorDom
-//     }
-
-//     const createMessageError = function(HTMLelementAppendTo, messageError){
-//         const pError = document.createElement('p');
-//         const textError = document.createTextNode(messageError);
-
-//         pError.appendChild(textError);
-
-//         HTMLelementAppendTo.appendChild(pError);
-//     }
-// }
-
 $(document).ready(function(){
-    $(":submit").click(function(e){
-        console.log('le bouton marche')
-        inputs.forEach((input,index) => {
-            if(input.name !== "valider"){
-                console.log('salut ça va')
-                if(input.value.trim() === ""){
-                    console.log('oui et toi ?')
-                    const divError = createDivError(container)
-                    createMessageError(divError, `Le champ ${input.name} n'a pas été remplis`)
-                }else{
 
-                }
-            }
-        })
+    const pseudo = $("#pseudo");
+    const mail = $("#mail");
+    const mdp = $("password");
+    const champ = $(".champ");
+    const valider = $("#valider");
+    const erreur = $(".erreur");
+    const reset = $("#reset")
+
+
+    function verifier(champ){
+        if(champ.val() == ""){
+            erreur.css('display', 'block');
+        }
+    }
+
+    valider.click(function(e){
         e.preventDefault();
+
+        verifier(pseudo);
+        verifier(mdp);
+        verifier(mail);
     });
 
-    const createDivError = function(HTMLelementAppendTo){
-        const divError = document.createElement('div')
-        $(".divError").attr("class", "error")
-
-        HTMLelementAppendTo.appendChild(divError)
-        HTMLelementAppendTo.classList.add('column-reverse')
-        const divErrorDom = document.querySelector('.error')
-        return divErrorDom
-    }
-
-    const createMessageError = function(HTMLelementAppendTo, messageError){
-        const pError = document.createElement('p');
-        const textError = document.createTextNode(messageError);
-
-        pError.appendChild(textError);
-
-        HTMLelementAppendTo.appendChild(pError);
-    }
+    reset.click(function(){
+        erreur.css('display', 'none')
+    })
 });
